f3edf774d8b4398e7c987c2c168b552539c10410
Maurizio Alessandri <maurizio.alessandri@virgilio.it>
2026-08-06 16:22:10 +0200

    Fixed bugs and resolved execution issues

--- BulkRNASeq/rsemstarindex/script originali/reformat.R
+++
@@ -1,6 +0,0 @@
-library(Biostrings)
-genome <- readDNAStringSet("/data/scratch/genome.fa", format="fasta")
-genome.bis <- genome[which(nchar(names(genome))< 60),]
-genome.bis <- genome.bis[setdiff(seq(1:length(names(genome.bis))),grep("^MT",names(genome.bis)))]
-writeXStringSet(genome.bis, "/data/scratch/genome.fa", format="fasta")
-
